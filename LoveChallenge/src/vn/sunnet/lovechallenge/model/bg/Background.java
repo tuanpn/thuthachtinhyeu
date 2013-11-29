045caf1700a843059db5dbb4746b212a59381cab
PhamNgoc <PhamNgoc@PhamNgocTuan>
2013-11-29 18:56:44 +0700

    project

--- LoveChallenge/src/vn/sunnet/lovechallenge/model/bg/Background.java
+++ LoveChallenge/src/vn/sunnet/lovechallenge/model/bg/Background.java
@@ -1,43 +1,47 @@
-//package vn.sunnet.lovechallenge.model.bg;
-//
-//import com.badlogic.gdx.math.Vector2;
-//
-//public class Background {
-//	
-//	private int id;
-//	
-//	private Vector2 position;
-//	
-//	private Vector2 velocity;
-//	
-//	public Background(int id) {
-//		this.id = id;
-//	}
-//
-//	public int getId() {
-//		return id;
-//	}
-//
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public Vector2 getPosition() {
-//		return position;
-//	}
-//
-//	public void setPosition(Vector2 position) {
-//		this.position = position;
-//	}
-//
-//	public Vector2 getVelocity() {
-//		return velocity;
-//	}
-//
-//	public void setVelocity(Vector2 velocity) {
-//		this.velocity = velocity;
-//	}
-//	
-//	
-//
-//}
+package vn.sunnet.lovechallenge.model.bg;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Background {
+
+	private int id;
+
+	private Vector2 position;
+
+	private Vector2 velocity;
+
+	public Background(int id) {
+		this.id = id;
+		velocity = new Vector2();
+		position = new Vector2();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
+	}
+
+	public void update(float delta) {
+		position.add(velocity.x * delta, velocity.y * delta);
+	}
+
+}
